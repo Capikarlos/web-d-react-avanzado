0dbc10decbec41f2b4dea8772007f7898545ed57
Capikarlos <lopezcarlosant@gmail.com>
2025-07-17 20:25:18 -0600

    servidorExpress

---
+++ express/src/app.js
@@ -0,0 +1,14 @@
+// Importamos el modulo express
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo')
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
