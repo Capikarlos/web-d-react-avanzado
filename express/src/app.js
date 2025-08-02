3e5682a2c25756db8e1a3e5f4a231bf70748db62
Capikarlos <lopezcarlosant@gmail.com>
2025-08-02 00:26:05 -0600

    recapitulando

--- express/src/app.js
+++ express/src/app.js
@@ -1,3 +1,19 @@
+// process.loadEnvFile()
+// esta es una opcion sin instalar nada
+
+// import { config } from 'dotenv'
+// config()
+/*
+Importamos el modulo dotenv
+Este modulo nos permite cargar variables de entorno desde un archivo .env que se encuentra en la raiz del proyecto
+*/
+require('dotenv').config()
+
+console.log(process.env.PORT)
+console.log(process.env.NOMBRE)
+/* console.log(process) */
+/* console.log(process.env) */
+
 // Importamos el modulo express
 const express = require('express')
 
