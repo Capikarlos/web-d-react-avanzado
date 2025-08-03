bae7a654ee8f39e70c6cb4d270982b6a18b839f2
Capikarlos <lopezcarlosant@gmail.com>
2025-08-02 20:02:15 -0600

    rutas express

--- express/src/app.js
+++ express/src/app.js
@@ -16,13 +16,65 @@ console.log(process.env.NOMBRE)
 
 // Importamos el modulo express
 const express = require('express')
+const { infoPeliculas } = require('./peliculas.js')
+console.log(infoPeliculas)
 
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo')
+})
+
+app.get('/api/peliculas', (req, res) => {
+  res.send(infoPeliculas)
+})
+
+app.get('/api/peliculas/accion', (req, res) => {
+  res.send(infoPeliculas.accion)
+})
+
+app.get('/api/peliculas/accion/year/:year', (req, res) => {
+  const year = parseInt(req.params.year)
+  const peliculasAccion = infoPeliculas.accion.filter(pelicula => pelicula.year === year)
+
+  if (peliculasAccion.length === 0) {
+    return res.status(404).send({ error: 'No se encontraron peliculas de accion para el año especificado' })
+  }
+
+  res.send(peliculasAccion)
+})
+
+app.get('/api/peliculas/accion/titulo/:titulo/:year', (req, res) => {
+  // const titulo = req.params.titulo
+  // const year = req.params.year
+  let { titulo, year } = req.params // Desestructuramos los parametros
+  year = parseInt(year) // Convertimos year a entero
+
+  const resultados = infoPeliculas.accion.filter(pelicula => pelicula.titulo === titulo && pelicula.year === year)
+  // const resultados = infoPeliculas.accion.filter(pelicula => pelicula.titulo.toLowerCase().includes(titulo.toLowerCase()))
+  // Alternativa para buscar en todas las categorias
+  // const resultados = Object.values(infoPeliculas).flat().filter(pelicula => pelicula.titulo.toLowerCase().includes(titulo.toLowerCase()))
+
+  if (resultados.length === 0) {
+    return res.status(404).send(`No se encontraron peliculas con el titulo: ${titulo} en el año: ${year}`)
+    // return res.status(404).send({ error: 'No se encontraron peliculas con el titulo especificado' })
+  }
+
+  res.send(resultados)
+})
+
+app.get('/api/peliculas/comedia/:pais', (req, res) => {
+  console.log(req.query.ordenar)
+  const pais = req.params.pais
+  const resultados = infoPeliculas.comedia.filter(pelicula => pelicula.pais === pais)
+
+  if (req.query.ordenar === 'year') {
+    return res.send(resultados.sort((a, b) => b.year - a.year))
+  }
+
+  res.send(resultados)
 })
 
 app.listen(PORT, () => {
